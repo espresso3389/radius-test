9226c0d4fef0467b0c873c889c8b2fcd1c79c6a5
Takashi Kawasaki <espresso3389@gmail.com>
2023-03-24 16:58:31 +0900

    CMV command is great!

--- bin/radius_test.dart
+++ bin/radius_test.dart
@@ -6,14 +6,17 @@ void main(List<String> arguments) async {
   final udp = await UDP.bind(Endpoint.any());
   udp.asStream().listen((datagram) {
     if (datagram == null) return;
-    print(ascii.decode(datagram.data));
+    final data = ascii
+        .decode(datagram.data)
+        .trim()
+        .split('\r')
+        .map((v) => double.parse(v));
+    print('count: ${data.length}');
+    print(data.join(','));
   });
-  final dest = Endpoint.unicast(InternetAddress.tryParse('192.168.0.100')!,
+  final dest = Endpoint.unicast(InternetAddress.tryParse('192.168.10.161')!,
       port: Port(48631));
 
-  await udp.send(ascii.encode('CS 2 65535\r'), dest);
-
-  for (int i = 0; i <= 65535; i++) {
-    await udp.send(ascii.encode('CS 1 $i\r'), dest);
-  }
+  //await udp.send(ascii.encode('CMV Get 0.1.CPGain.{I1O1:I1O99}\r'), dest);
+  await udp.send(ascii.encode('CMV Get 0.1.CPGain.{I1O1:I2O99}\r'), dest);
 }
